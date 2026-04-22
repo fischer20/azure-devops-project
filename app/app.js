ea4d9cdddba38c9d8c03e299b6f3c44971d3fb65
Fischer KB <bankazifischer85@gmail.com>
2026-04-22 23:45:31 +0200

    change readme

--- app/app.js
+++ app/app.js
@@ -1,12 +1,34 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const port = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello DevOps 🚀');
+  let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+
+  const ext = path.extname(filePath);
+
+  const contentType = {
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'application/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg'
+  }[ext] || 'text/plain';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('404 Not Found');
+      return;
+    }
+
+    res.writeHead(200, { 'Content-Type': contentType + '; charset=utf-8' });
+    res.end(content);
+  });
 });
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
-}); 
+});
